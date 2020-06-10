b198894bed399a459310ca802b0fb24c38508935
muscaiu <muscaiu@yahoo.com>
2020-06-10 14:42:16 +0300

    3.0.0

--- main.js
+++ main.js
@@ -50,3 +50,7 @@ autoUpdater.on('update-downloaded', () => {
   console.log('update-downloaded');
   mainWindow.webContents.send('update_downloaded');
 });
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
